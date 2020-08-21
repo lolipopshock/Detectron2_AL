55816322d1c5f0b690c213c019c5a7aee162a60a
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-08-21 11:59:21 -0400

    Update AL model configs

--- src/detectron2_al/configs/defaults.py
+++ src/detectron2_al/configs/defaults.py
@@ -8,6 +8,10 @@ from detectron2.config.config import CfgNode as CN
 _C.AL = CN()
 _C.AL.MODE = 'object' # {'image', 'object'}
 # Perform active learning on whether image-level or object-level 
+_C.AL.OBJECT_SCORING = '1vs2' # {'1vs2, 'least_confidence', 'jitter'}
+# The method to compute the individual object scores  
+_C.AL.IMAGE_SCORE_AGGREGATION = 'avg' # {'avg', 'max', 'sum'}
+# The method to aggregate the individual object scores to the whole image score 
 
 _C.AL.DATASET = CN()
 # Specifies the configs for creating new datasets 
@@ -25,33 +29,23 @@ _C.AL.DATASET.OBJECT_BUDGET = 2000
 _C.AL.DATASET.SAMPLE_METHOD = 'top' # {'top', 'kmeans'}
 # The method to sample images when labeling 
 
-_C.AL.IMAGE = CN()
-# Specifies the configs for image level active learning
-_C.AL.IMAGE.INDIVIDUAL_SCORE = '1vs2' # {}
-# The method to compute the individual object scores  
-_C.AL.IMAGE.IMAGE_AGGREGATION = 'avg' # {'avg',  'max', 'sum'}
-# The method to aggregate the individual object scores to the whole image score 
-
-_C.AL.OBJECT = CN()
-# Specifies the configs for object level active learning
-_C.AL.OBJECT.INDIVIDUAL_SCORE = '1vs2' # {}
-_C.AL.OBJECT.FUSION = CN()
+_C.AL.OBJECT_FUSION = CN()
 # Specifies the configs to fuse model prediction and ground-truth (gt)
-_C.AL.OBJECT.FUSION.OVERLAPPING_METRIC = 'iou' # {'iou', 'dice_coefficient', 'overlap_coefficient'}
+_C.AL.OBJECT_FUSION.OVERLAPPING_METRIC = 'iou' # {'iou', 'dice_coefficient', 'overlap_coefficient'}
 # The function to calculate the overlapping between model pred and gt
-_C.AL.OBJECT.FUSION.OVERLAPPING_TH = 0.25
+_C.AL.OBJECT_FUSION.OVERLAPPING_TH = 0.25
 # The threshold for selecting the boxes 
-_C.AL.OBJECT.FUSION.SELECTION_METHOD = 'iou' # {'top', 'above', 'nonzero'}
+_C.AL.OBJECT_FUSION.SELECTION_METHOD = 'iou' # {'top', 'above', 'nonzero'}
 # For gt boxes with non-zero overlapping with the pred box, specify the 
 # way to choose the gt boxes. 
 # top: choose the one with the highest overlapping
 # above: choose the ones has the overlapping above the threshold specified above
 # nonzero: choose the gt boxes with non-zero overlapping
-_C.AL.OBJECT.FUSION.REMOVE_DUPLICATES = True
-_C.AL.OBJECT.FUSION.DUPLICATE_TH = 0.25
+_C.AL.OBJECT_FUSION.REMOVE_DUPLICATES = True
+_C.AL.OBJECT_FUSION.REMOVE_DUPLICATEs_TH = 0.25
 # For the fused dataset, remove duplicated boxes with overlapping more than 
 # the given threshold
-_C.AL.OBJECT.FUSION.RECOVER_MISSING = True
+_C.AL.OBJECT_FUSION.RECOVER_MISSING = True
 # If true, we recover the mis-identified objects during the process
 
 _C.AL.TRAINING = CN()
