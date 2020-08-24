f2f463cb7d9d7dcd9d79200427aaf4d6fc32b5b4
loliipopshock <22512825+lolipopshock@users.noreply.github.com>
2020-08-24 00:53:30 -0400

    Update default configuration * Optimize the dataset and training settings

--- src/detectron2_al/configs/defaults.py
+++ src/detectron2_al/configs/defaults.py
@@ -17,7 +17,19 @@ _C.AL.DATASET = CN()
 # Specifies the configs for creating new datasets 
 # It will also combines configs from DATASETS and DATALOADER 
 # when creating the DynamicDataset for training.
-_C.AL.DATASET.BUDGET_STYLE = 'object'
+_C.AL.DATASET.NAME = ''
+_C.AL.DATASET.IMG_ROOT = ''
+_C.AL.DATASET.ANNO_PATH = ''
+# Extra meta information for the dataset, only supports COCO Dataset
+# This is somehow ugly, and shall be updated in the future.
+_C.AL.DATASET.CACHE_DIR = 'al_datasets'
+# The created dataset will be saved in during the active learning 
+# training process 
+_C.AL.DATASET.NAME_PREFIX = 'r'
+# The created dataset will be named as '{NAME}-{NAME_PREFIX}{idx}' in the 
+# Detectron2 system
+_C.AL.DATASET.BUDGET_STYLE = 'object' # {'image', 'object'}
+# Depericated. Now the budget style is the same as AL.MODE
 _C.AL.DATASET.IMAGE_BUDGET = 20
 _C.AL.DATASET.OBJECT_BUDGET = 2000
 # Specifies the way to calculate the budget 
@@ -26,6 +38,7 @@ _C.AL.DATASET.OBJECT_BUDGET = 2000
 # by OBJECT_BUDGET = IMAGE_BUDGET * AVG_OBJ_IN_TRAINING. 
 # Similarly, we have 
 # IMAGE_BUDGET = OBJECT_BUDGET // AVG_OBJ_IN_TRAINING.
+_C.AL.DATASET.BUDGET_ALLOCATION = 'linear'
 _C.AL.DATASET.SAMPLE_METHOD = 'top' # {'top', 'kmeans'}
 # The method to sample images when labeling 
 
@@ -49,7 +62,11 @@ _C.AL.OBJECT_FUSION.RECOVER_MISSING_OBJECTS = True
 # If true, we recover the mis-identified objects during the process
 
 _C.AL.TRAINING = CN()
-_C.AL.TRAINING.NUM_EPOCHS = 5
-_C.AL.TRAINING.UPDATE_FREQ = 3
-_C.AL.TRAINING.MERGING_RATIO_INIT = 0.95
-_C.AL.TRAINING.MERGING_RATIO_DECAY = 'exp'
+_C.AL.TRAINING.ROUNDS = 5 
+# The number of rounds for performing AL dataset update
+_C.AL.TRAINING.EPOCHS_PER_ROUND = 5
+# The numbers of epochs for training during each round. 
+# As Detectron2 does not support epochs natively, we will use the 
+# following formula to convert the epochs to iterations after creating 
+# the new dataset:
+# iterations = total_imgs / batch_size * epochs_per_round
